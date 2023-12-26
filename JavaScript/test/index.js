0f86a1c06837a1e0650fff89f3c4df15627bb3af
HiLow <newbie_wisdom@hotmail.com>
2023-12-26 13:58:38 +0900

    update

--- JavaScript/test/index.js
+++ JavaScript/test/index.js
@@ -1,11 +1,11 @@
-const { convertFromCharacterEntityReference, convertToCharacterEntityReference } = require('../lib/character-entity-reference.js');
+const CharacterEntityReference = require('../lib/character-entity-reference.js');
 
 const text = '&quot;abcdef&quot;';
 const text2 = '\\';
 
 try {
-    const result = convertFromCharacterEntityReference(text);
-    const result2 = convertToCharacterEntityReference(text2, true);
+    const result = CharacterEntityReference.convertFromCharacterEntityReference(text);
+    const result2 = CharacterEntityReference.convertToCharacterEntityReference(text2, true);
 
     console.log(result);
     console.log(result2);
